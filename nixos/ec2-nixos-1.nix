fbc718a401c26ad22885cd6cfca8f8a190727d93
Aristid Breitkreuz <aristidb@gmail.com>
2012-12-04 02:35:07 +0100

    more nixos config

--- nixos/ec2-nixos-1.nix
+++ nixos/ec2-nixos-1.nix
@@ -1,7 +1,19 @@
 { config, pkgs, modulesPath, ... }:
 
+let sshKeyFiles = [ ../ssh/mba_rsa.pub ]; in
 {
   require = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
+
+  users.extraUsers = { root = { openssh.authorizedKeys.keyFiles = sshKeyFiles; }; };
+
+  time.timeZone = "Europe/Berlin";
+
+  services.openssh = { enable = true;
+  		       passwordAuthentication = false;
+		       permitRootLogin = "without-password";
+		       #ports = [ 22 ];
+  		     };
+
   environment.systemPackages = with pkgs; [ screen jre emacs vim ];
-  
+  environment.enableBashCompletion = true;
 }
