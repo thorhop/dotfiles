88b4c2d8ff80d1adadcf7b8118e21152428de97d
Aristid Breitkreuz <aristidb@gmail.com>
2012-12-04 20:36:41 +0100

    cleanup and spacing

--- nixos/hermit-ec2.nix
+++ nixos/hermit-ec2.nix
@@ -5,32 +5,32 @@ let sshKeyFiles = [ ../ssh/mba_rsa.pub ]; in
   require = [ "${modulesPath}/virtualisation/amazon-image.nix" ];
 
   fileSystems = [ { mountPoint = "/mnt/irc-vol";
-                    device = "/dev/disk/by-label/irc-vol";
+                    label = "irc-vol";
                   } ];
 
   users.extraUsers = { root = { openssh.authorizedKeys.keyFiles = sshKeyFiles; };
                        aristid = { createUser = true;
-		       	       	   createHome = true;
-				   description = "Aristid Breitkreuz";
-				   group = "users";
-				   extraGroups = [ "wheel" ];
-				   home = "/home/aristid";
-				   isSystemUser = false;
-				   useDefaultShell = true;
-				   openssh.authorizedKeys.keyFiles = sshKeyFiles;
-		       	         };
-		     };
+                               	   createHome = true;
+                                   description = "Aristid Breitkreuz";
+                                   group = "users";
+                                   extraGroups = [ "wheel" ];
+                                   home = "/home/aristid";
+                                   isSystemUser = false;
+                                   useDefaultShell = true;
+                                   openssh.authorizedKeys.keyFiles = sshKeyFiles;
+                                 };
+                     };
 
   time.timeZone = "Europe/Berlin";
   networking.hostName = "hermit.breitkreuz.me";
 
   security.sudo = { enable = true;
-  		    wheelNeedsPassword = false;
+                    wheelNeedsPassword = false;
   		  };
 
   services.openssh = { enable = true;
-  		       passwordAuthentication = false;
-		       ports = [ 22023 ];
+                       passwordAuthentication = false;
+                       ports = [ 22023 ];
   		     };
 
   environment.systemPackages = with pkgs; [ which screen jre emacs vim git ];
