26b08d99388c5e017a14eaee95d4d2bb6e5bd105
Aristid Breitkreuz <aristidb@gmail.com>
2012-12-18 01:48:26 +0100

    make pseudo-haskell-platform expression

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,28 +1,32 @@
 pkgs : {
-  packageOverrides = pkgs : rec {
+  packageOverrides = pkgs :
+    let pseudoHaskellPlatform = hask : with hask;
+        if builtins.currentSystem == "x86_64-darwin" then
+        [ HTTP HUnit QuickCheck async cgi fgl
+          haskellSrc html network parallel parsec primitive
+          regexBase regexCompat regexPosix
+          split stm syb deepseq text transformers mtl vector xhtml zlib random
+          cabalInstall alex happy ghc haddock ]
+        else
+        [ hask.haskellPlatform ];
+    in
+  rec {
     ghc74Darwin = {
-      all = pkgs.haskellPackages_ghc742.ghcWithPackages (self : with self; [
-#          HTTP HUnit QuickCheck async cgi fgl
-#          haskellSrc html network parallel parsec primitive
-#          regexBase regexCompat regexPosix
-#          split stm syb deepseq text transformers mtl vector xhtml zlib random
-#          tar transformers
-          cabalInstall alex happy ghc haddock
-          cabalDev
+      all = pkgs.haskellPackages_ghc742.ghcWithPackages (self : with self; 
+          pseudoHaskellPlatform self ++
+          [
+            criterion
+            testFramework
+            testFrameworkHunit
+            testFrameworkQuickcheck2
+            testFrameworkTh
+            hspec
 
-          QuickCheck
-          criterion
-          testFramework
-          testFrameworkHunit
-          testFrameworkQuickcheck2
-          testFrameworkTh
-          hspec
-
-          httpConduit
-          attempt
-          cryptohash
-          xmlConduit
-      ]);
+            httpConduit
+            attempt
+            cryptohash
+            xmlConduit
+          ]);
     };
 
     envGhc74Darwin = pkgs.myEnvFun {
