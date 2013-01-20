2cb11108996597eb9986a4d2dd430d6d82be4b1b
Aristid Breitkreuz <aristidb@gmail.com>
2013-01-20 22:44:29 +0100

    modular haskell

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -3,93 +3,41 @@ pkgs : {
   #pulseaudio = true;
 
   packageOverrides = pkgs :
-    let pseudoHaskellPlatform = hask : with hask;
-        if builtins.currentSystem == "x86_64-darwin" then
-        [ HTTP HUnit QuickCheck async cgi fgl
-          haskellSrc html network parallel parsec primitive
-          regexBase regexCompat regexPosix
-          split stm syb deepseq text transformers mtl vector xhtml zlib random
-          cabalInstall alex happy ghc haddock ]
-        else
-        [ hask.haskellPlatform ];
-        
-        ghcAws = hask : hask.ghcWithPackages (self : with self;
-             [
-               cabalInstall
-               attempt
-               base64Bytestring
-               blazeBuilder
-               caseInsensitive
-               cereal
-               conduit
-               cryptoApi
-               cryptohash
-               httpConduit
-               httpTypes
-               liftedBase
-               monadControl
-               mtl
-               resourcet
-               utf8String
-               xmlConduit
-             ]);
+    let haskellGen = import ./haskell.nix pkgs;
+        haskell = haskellGen (pkgs.haskellPackages_ghc742);
+        haskellGhc76 = haskellGen (pkgs.haskellPackages_ghc761);
     in
   rec {
-    ghc74Def = pkgs.haskellPackages_ghc742.ghcWithPackages (self : with self; 
-          pseudoHaskellPlatform self ++
-          [
-            Agda
-
-            criterion
-            testFramework
-            testFrameworkHunit
-            testFrameworkQuickcheck2
-            testFrameworkTh
-            hspec
-          ]);
-
-    ghc74Aws = ghcAws pkgs.haskellPackages_ghc742;
-    ghc76Aws = ghcAws pkgs.haskellPackages_ghc761;
-
-    envGhc74Def = pkgs.myEnvFun {
-      name = "ghc74-def";
-      buildInputs = [ ghc74Def ];
-    };
-
-    envGhc74Aws = pkgs.myEnvFun {
-      name = "ghc74-aws";
-      buildInputs = [ ghc74Aws ];
-    };
-
-    envGhc76Aws = pkgs.myEnvFun {
-      name = "ghc76-aws";
-      buildInputs = [ ghc76Aws ];
-    };
-
-    envGhcHEADAws = pkgs.myEnvFun {
-      name = "ghcHEAD-aws";
-      buildInputs = [ (ghcAws pkgs.haskellPackages_ghcHEAD) ];
-    };
-
-    envGhc74Stackage = pkgs.myEnvFun {
-      name = "ghc74-stackage";
-      buildInputs = with pkgs.haskellPackages_ghc742;
-                    [ (ghcWithPackages (self : [ self.Cabal_1_16_0_3 ])) cabalInstall_1_16_0_2 cabalDev ];
-    };
-
     osxEnv = pkgs.buildEnv {
       name = "osx-env";
-      paths = [ pkgs.nix ghc74Def envGhc74Aws envGhc76Aws envGhc74Stackage pkgs.bup ];
+      paths = with pkgs; [ nix
+                           haskell.ghcDef
+                           haskell.envGhcAws
+                           haskellGhc76.envGhcAws
+                           haskell.envGhcStackage
+                           bup ];
     };
 
     hermitEnv = pkgs.buildEnv {
       name = "hermit-env";
-      paths = [ ghc74Def envGhc74Aws envGhc76Aws pkgs.weechat pkgs.ncdu ];
+      paths = with pkgs; [ haskell.ghcDef
+                           haskell.envGhcAws
+                           haskellGhc76.envGhcAws
+                           pkgs.weechat
+                           pkgs.ncdu ];
     };
 
     medusaEnv = pkgs.buildEnv {
       name = "medusa-env";
-      paths = with pkgs; [ dropbox keepassx minecraft unetbootin evince pavucontrol ];
+      paths = with pkgs; [ dropbox
+                           keepassx
+                           minecraft
+                           unetbootin
+                           evince
+                           pavucontrol
+                           haskell.ghcDef
+                           haskell.envGhcAws
+                           haskellGhc76.envGhcAws ];
     };
   };
 }
---
+++ nixpkgs/haskell.nix
@@ -0,0 +1,67 @@
+pkgs : hask :
+
+rec {
+  version = hask.ghcPlain.version;
+
+  pseudoHaskellPlatform = with hask;
+        if builtins.currentSystem == "x86_64-darwin" then
+        [ HTTP HUnit QuickCheck async cgi fgl
+          haskellSrc html network parallel parsec primitive
+          regexBase regexCompat regexPosix
+          split stm syb deepseq text transformers mtl vector xhtml zlib random
+          cabalInstall alex happy ghc haddock ]
+        else
+        [ haskellPlatform ];
+
+  ghcDef = hask.ghcWithPackages (self : with self;
+        pseudoHaskellPlatform ++
+        [
+          Agda
+
+          criterion
+          testFramework
+          testFrameworkHunit
+          testFrameworkQuickcheck2
+          testFrameworkTh
+          hspec
+        ]);
+
+  ghcAws = hask.ghcWithPackages (self : with self;
+        [
+          cabalInstall
+          attempt
+          base64Bytestring
+          blazeBuilder
+          caseInsensitive
+          cereal
+          conduit
+          cryptoApi
+          cryptohash
+          httpConduit
+          httpTypes
+          liftedBase
+          monadControl
+          mtl
+          resourcet
+          utf8String
+          xmlConduit
+        ]);
+
+  envGhcDef = pkgs.myEnvFun {
+    name = "ghc-def-${version}";
+    buildInputs = [ ghcDef ];
+  };
+
+  envGhcAws = pkgs.myEnvFun {
+    name = "ghc-aws-${version}";
+    buildInputs = [ ghcAws ];
+  };
+
+  envGhcStackage = pkgs.myEnvFun {
+    name = "ghc-stackage-${version}";
+    buildInputs = with hask;
+                  [ (ghcWithPackages (self : [ self.Cabal_1_16_0_3 ]))
+                    cabalInstall_1_16_0_2
+                    cabalDev ];
+  };
+}
