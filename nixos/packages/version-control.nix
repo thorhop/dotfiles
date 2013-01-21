2507b3bd40527a45c392403565b20c4adbbcfdd3
Aristid Breitkreuz <aristidb@gmail.com>
2013-01-21 23:57:07 +0100

    it's gitSVN not gitSvn...

--- nixos/packages/version-control.nix
+++ nixos/packages/version-control.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 
-let myGit = if config.services.xserver.enable then pkgs.gitFull else pkgs.gitSvn; in
+let myGit = if config.services.xserver.enable then pkgs.gitFull else pkgs.gitSVN; in
 {
   environment.systemPackages = with pkgs; [ myGit subversion mercurial ];
 }
