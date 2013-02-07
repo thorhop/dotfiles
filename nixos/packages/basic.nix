a608724e8b323c4ad627736234e08c912639c02a
Aristid Breitkreuz <aristidb@gmail.com>
2013-02-08 00:52:11 +0100

    use pstree from psmisc

--- nixos/packages/basic.nix
+++ nixos/packages/basic.nix
@@ -1,5 +1,5 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ which wget file pstree psmisc pv screen ];
+  environment.systemPackages = with pkgs; [ which wget file psmisc pv screen ];
 }
