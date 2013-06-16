41f02c4a37cf02a7f7f6ca631cf85a637756037f
Aristid Breitkreuz <aristidb@gmail.com>
2013-06-16 18:06:38 +0200

    git config generation script, global gitignore

---
+++ gitconfig.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+git config --global user.name "Aristid Breitkreuz"
+git config --global user.email "aristidb@gmail.com"
+git config --global push.default "simple"
+git config --global core.excludesfile ~/dotfiles/gitignore-global
