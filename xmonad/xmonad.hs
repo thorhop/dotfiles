0c2153329e65d0510b87d37846d58ddce3ed8b85
Aristid Breitkreuz <aristidb@gmail.com>
2013-06-22 15:08:26 +0200

    xmonad: fullscreen youtube videos

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,11 +1,14 @@
 -- vim: ai:et:sw=4:sts=4:
 import XMonad
+import qualified XMonad.StackSet as W
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 import XMonad.Util.Run(spawnPipe, runInTerm)
 import XMonad.Util.EZConfig(additionalKeysP)
 import qualified XMonad.Prompt as P
 import qualified XMonad.Prompt.Shell as P
+import XMonad.Layout.NoBorders (smartBorders)
 import System.IO
 
 myterminal = "urxvt"
@@ -13,8 +16,8 @@ myterminal = "urxvt"
 main = do xmobarp <- spawnPipe "xmobar"
           xmonad $ defaultConfig
               { modMask = mod4Mask -- "Windows" key
-              , manageHook = manageDocks <+> manageHook defaultConfig
-              , layoutHook = avoidStruts $ layoutHook defaultConfig
+              , manageHook = manageDocks <+> manageHook defaultConfig <+> (isFullscreen --> doF W.focusDown <+> doFullFloat)
+              , layoutHook = smartBorders $ avoidStruts $ layoutHook defaultConfig
               , logHook = dynamicLogWithPP xmobarPP
                             { ppOutput = hPutStrLn xmobarp
                             , ppTitle = xmobarColor "green" "" . shorten 80
