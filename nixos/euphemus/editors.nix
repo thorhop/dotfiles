647f3ae565cc1cbe2c2a326f93f61508ac397e77
Aristid Breitkreuz <aristidb@gmail.com>
2013-07-14 15:33:44 +0200

    aspell is a bit problematic right now and i do not need it at this time

--- nixos/euphemus/editors.nix
+++ nixos/euphemus/editors.nix
@@ -4,9 +4,5 @@
   environment.systemPackages = with pkgs; [
       emacs
       (vim_configurable.override { source = "latest"; })
-      aspell
-      aspellDicts.de
-      aspellDicts.en
-      aspellDicts.nl
     ];
 }
