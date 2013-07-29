62a02d9e676c54352c3171e911c84c1ef7c360c5
Aristid Breitkreuz <aristidb@gmail.com>
2013-07-29 22:57:07 +0200

    color.ui=auto

--- gitconfig.sh
+++ gitconfig.sh
@@ -3,6 +3,7 @@ git config --global user.name "Aristid Breitkreuz"
 git config --global user.email "aristidb@gmail.com"
 git config --global push.default simple
 git config --global core.excludesfile ~/dotfiles/gitignore-global
+git config --global color.ui auto
 git config --global color.diff auto
 git config --global color.status auto
 git config --global color.branch auto
