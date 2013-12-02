317692b8ce0b10cb7059da85c6f48435ab9f9407
Aristid Breitkreuz <aristidb@gmail.com>
2013-12-02 23:24:25 +0100

    gc-keep-outputs

--- nixos/common/nix-cfg.nix
+++ nixos/common/nix-cfg.nix
@@ -3,7 +3,11 @@
 {
   nix = { useChroot = true;
           readOnlyStore = true;
-          extraOptions = "build-cores = 0"; };
+          extraOptions = ''
+            build-cores = 0
+            gc-keep-outputs = true
+            gc-keep-derivations = true
+          ''; };
 
   nixpkgs = { config = import ../../nixpkgs/config.nix; };
 }
